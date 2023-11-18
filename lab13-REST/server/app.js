9194e0f1abae5f5f84ac982be00f792f30d4ab0a
ThiriMon1 <108010913+ThiriMon1@users.noreply.github.com>
2023-11-18 16:55:13 -0600

    update app.js

--- lab13-REST/server/app.js
+++ lab13-REST/server/app.js
@@ -7,4 +7,8 @@ app.use(express.json());
 
 app.use('/books',bookRouter);
 
+app.use((err,req,res,next)=>{
+    res.status(500).send(`Server Error ${err.message}`);
+});
+
 app.listen(3000, ()=> console.log('listening on 3000'));
